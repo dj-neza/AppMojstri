eb8447e8888943af72c54ed6f4e6ba85a1e720b0
Patricija Brečko <pb3238@student.uni-lj.si>
2016-07-12 11:54:01 +0200

    preimenovanje v php

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     <head>
         <meta charset="utf-8"> 
         <title>Dr. Datum</title>
-        <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
     <body>
         <div class="container">
-          <div class="jumbotron hudo-ozadje" align="center">
+          <div class="jumbotron ozadje" align="center">
             <img src="icon_brainstorm.png" class="img-rounded" width="90" height="90">
-            <h1>Dr. Datum</h1>
-            <p>Naročanje še nikoli ni bilo hitrejše in enostavnejše kot takrat, kadar je na izmeni Dr. Datum!</p>
-            <p> Izberi, potrdi, pridi!</p>
+            <h1 class="change-font dr_datum">Dr. Datum</h1>
+            <p class="change-font">Naročanje še nikoli ni bilo hitrejše in enostavnejše kot takrat, kadar je na izmeni Dr. Datum!</p>
+            <p class="change-font"> Izberi, potrdi, pridi!</p>
           </div>
           
           <div class="row">
@@ -55,8 +55,8 @@
         
         
         
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-        <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
         <script src="js/app.js"></script>
     </body>
 
