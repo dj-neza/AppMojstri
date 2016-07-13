5770f1787f5b8d99659277140b0dfb75ac4b53be
Gregor Leskovšek <legrega@gmail.com>
2016-07-13 11:56:34 +0200

    narocila 2 dummy objekta v polju

--- getdatafromdb.inc.php
+++ getdatafromdb.inc.php
@@ -10,7 +10,7 @@ $stmt->execute();
 $stmt->bind_result($wuq_id, $security_question_id, $answer);
 $stmt->store_result();
 */
-$num_rows = 1;//$stmt->num_rows;
+$num_rows = 2;//$stmt->num_rows;
 $i = 0;
 $ime = 'Janez';
 $priimek = 'Novak';
@@ -23,6 +23,32 @@ $ura = '11:30:00';
 $uraMin = substr($ura, 0, strlen($ura) -3);
 
 echo '<script>let narocila = [';
+while($i < $num_rows-1) {
+	if (/*$stmt->fetch()*/true) {
+		echo '{ime:"' . $ime . '",' .
+			'priimek:"' . $priimek . '",' .
+			'mail:"' . $mail . '",' .
+			'razlog:"' . $razlog . '",' .
+			'datum:"' . $datumSI . '",' .
+			'ura:"' . $uraMin . '"}';
+
+		if (++$i != $num_rows) {
+			echo ',';
+		}
+
+	}
+}	
+
+$ime = 'Jure';
+$priimek = 'Nagy';
+$mail = 'jure@heci.si';
+$razlog = 'zob me boli';
+$datum = '2016-07-18';
+$datumSI = DateTime::createFromFormat('Y-m-d', $datum);
+$datumSI = $datumSI->format('d.m.Y');
+$ura = '9:00:00';
+$uraMin = substr($ura, 0, strlen($ura) -3);
+
 while($i < $num_rows) {
 	if (/*$stmt->fetch()*/true) {
 		echo '{ime:"' . $ime . '",' .
