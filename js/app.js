502476670d0517bc8c0ca0b630efa8784b5b1687
PatricijaB <pb3238@student.uni-lj.si>
2016-07-14 15:33:39 +0000

    first commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,192 @@
 var main = function() {
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
         header: {
-         left   : 'prev,next',
-         center : 'title',
-         right  : 'agendaDay, agendaWeek',
-        },
+			left: 'title',
+			center: 'agendaDay,agendaWeek,month',
+			right: 'prev,next today'
+		},
+		editable: true,
+		selectable: true,
+			
         weekends: false, // will hide Saturdays and Sundays
         defaultView: 'agendaWeek',
-        slotDuration: '00:10:00',
+        slotDuration: '00:20:00',
         minTime: '07:00:00',
-        maxTime: '19:00:00'
+        maxTime: '19:00:00',
+        
+        axisFormat: 'h:mm',
+		columnFormat: {
+            month: 'ddd',    // Mon
+            week: 'ddd d', // Mon 7
+            day: 'dddd M/d',  // Monday 9/7
+            agendaDay: 'dddd d'
+        },
+        titleFormat: {
+            month: 'MMMM yyyy', // September 2009
+            week: "MMMM yyyy", // September 2009
+            day: 'MMMM yyyy' // Tuesday, Sep 8, 2009
+        },
+		allDaySlot: false,
+		
+        eventSources: [
+        // delavnik
+        {
+            events: [
+                {
+                    title  : '',
+                    start  : '2016-07-11T07:00:00',
+                    end    : '2016-07-11T14:00:00',
+                    allDay: false
+                },
+                {
+                    title  : '',
+                    start  : '2016-07-12T07:00:00',
+                    end    : '2016-07-12T14:00:00',
+                    allDay: false
+                    
+                },
+                {
+                    title  : '',
+                    start  : '2016-07-13T12:00:00',
+                    end    : '2016-07-13T19:00:00',
+                    allDay: false
+                },
+                {
+                    title  : '',
+                    start  : '2016-07-15T07:00:00',
+                    end    : '2016-07-15T14:00:00',
+                    allDay: false
+                }
+            ],
+            color: '#5bc0de',
+            textColor: '#103d47'
+            
+        },
+        // dopust
+        {
+            events: [
+                {
+                    title  : 'DOPUST',
+                    start  : '2016-07-14T07:00:00',
+                    end    : '2016-07-14T19:00:00',
+                    allDay: false
+                }
+            ],
+            color: '#f1f1f2',
+            textColor: '#545454'
+        },
+        // narocila
+        {
+            events: [
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T07:10:00',
+                    end    : '2016-07-11T07:40:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T07:50:00',
+                    end    : '2016-07-11T08:20:00',
+                    allDay: false
+                    
+                },
+                {
+                    title  : 'Sestanek',
+                    start  : '2016-07-11T09:30:00',
+                    end    : '2016-07-11T10:30:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T11:20:00',
+                    end    : '2016-07-11T11:40:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T11:50:00',
+                    end    : '2016-07-11T12:00:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T12:30:00',
+                    end    : '2016-07-11T12:50:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T13:00:00',
+                    end    : '2016-07-11T13:20:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-11T13:30:00',
+                    end    : '2016-07-11T13:50:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Sistematski pregledi',
+                    start  : '2016-07-12T08:00:00',
+                    end    : '2016-07-12T14:00:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-13T14:10:00',
+                    end    : '2016-07-13T14:30:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-13T14:50:00',
+                    end    : '2016-07-13T15:10:00',
+                    allDay: false
+                    
+                },
+                {
+                    title  : 'Sestanek',
+                    start  : '2016-07-13T15:30:00',
+                    end    : '2016-07-13T16:30:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-13T17:20:00',
+                    end    : '2016-07-13T17:40:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-13T17:50:00',
+                    end    : '2016-07-13T18:00:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-13T18:30:00',
+                    end    : '2016-07-13T18:50:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Pregled',
+                    start  : '2016-07-13T12:10:00',
+                    end    : '2016-07-13T12:30:00',
+                    allDay: false
+                },
+                {
+                    title  : 'Sistematski pregledi',
+                    start  : '2016-07-15T08:00:00',
+                    end    : '2016-07-15T14:00:00',
+                    allDay: false
+                }
+            ],
+            color: '#dda299',
+            textColor: '#f76346'
+        }
+        ]
     });
 
     prikaz_marocil(narocila);
@@ -101,7 +277,7 @@ var narocila = [
         ime: 'Marko',
         priimek: 'Zeman',
         mail: 'markozeman@gmail.com',
-        razlog: 'biubsuzbsuzbduzddudzusvdszvduvdutdvatvdtuadb8zfg7rvuf',
+        razlog: 'Pozdravljeni! Včeraj sem igral nogomet in grdo padel. Rad bi prišel na pregled kolena.',
         datum: '12.07.2016',
         ura: '15:00'
     },    
@@ -109,7 +285,7 @@ var narocila = [
         ime: 'Patricija',
         priimek: 'Brečko',
         mail: 'patra@gmail.com',
-        razlog: 'biubsuzbsuzbduzddudzusvdszvduvdugfdfdhdhfhhgghfoiiiciniundtdvatvdtuadb8zfg7rvuf',
+        razlog: 'Pozdravljeni, spet mi je zmanjkalo železa. Mislim, da potrebujem samo napotnico za laboratorij, da mi pregledajo kri.',
         datum: '13.07.2016',
         ura: '16:00'
     },  
@@ -117,7 +293,15 @@ var narocila = [
         ime: 'Neža',
         priimek: 'Đ',
         mail: 'neđa@gmail.com',
-        razlog: 'POPODekjbuinbiubsuzbsuzbduzddudzusvdszvduvdugfdfdhdhfhhgghfoiiiciniundtdvatvdtuadb8zfg7rvuf',
+        razlog: 'Pozdravljeni! Imam vročino, boli me glava, smrkam, kašljam, kiham, ko se premikam pa čutim tudi bolečine v želodcu. Zdi se mi, da je moje stanje precej nujno, zato bi se čimprej naročila na pregled.',
+        datum: '15.07.2016',
+        ura: '17:00'
+    },
+    {
+        ime: 'Neža',
+        priimek: 'Đ',
+        mail: 'neđa@gmail.com',
+        razlog: 'Res me boli!!.',
         datum: '15.07.2016',
         ura: '17:00'
     }
@@ -131,8 +315,12 @@ function prikaz_marocil (narocila) {
         var razlog = narocila[i].razlog;
         var datum = narocila[i].datum;
         var ura =  narocila[i].ura;
+        if (i%2 == 0) {
+            $('#narocilnice').append('<div class="row">');
+        }
         $('#narocilnice').append(
-        '<div class="panel panel-info odmik"> \
+        '<div class="col-md-6"> \
+        <div class="panel panel-info odmik"> \
             <div class="panel-heading">'+ ime_priimek +'</div> \
             <div class="panel-body" id="podrobno"> \
                 <p class="change-font">'+ razlog +'</p> \
@@ -150,7 +338,10 @@ function prikaz_marocil (narocila) {
                 </button> \
                 <p class="text-danger change-font opozorilo" style="display: none;"> Izpolnite pričakovano trajanje pregleda. </p> \
              </div> \
-        </div>');
+        </div></div>');
+        if (i%2 == 0) {
+            $('#narocilnice').append('</div>');
+        }
     }
 }
 
--- zdravnik.php
+++ zdravnik.php
@@ -15,14 +15,18 @@
     <body>
         <div class="container">
             <div class="row">
-              <div class="col-md-6">   
-                  <select class="form-control input-mini drop_down" id="zd">
+                <div class="col-md-4">
+                <img align="left" src="ikona_podstran.png" class="img-rounded" width="140" height="70">
+                </div>
+                <div class="col-md-4">
+                <select class="form-control input-mini drop_down" id="zd">
                     <option value="" disabled selected> Vaš zdravsteni dom </option>
                     <option id="zd1" value="bezigrad">ZD Ljubljana - enota Bežigrad</option>
                     <option id="zd2" value="moste">ZD Ljubljana - enota Moste-Polje</option>
                     <option id="zd3" value="sentvid">ZD Ljubljana - enota Šentvid </option>
                   </select>
-                  
+                  </div>
+                <div class="col-md-4">
                   <select class="form-control input-mini drop_down" id="zd_zdravnik">
                     <option value="" disabled selected> Zdravnik </option>
                     <option id="zdr1" value="bezigrad1"> dr. med. Mojca Horvat </option>
@@ -32,17 +36,39 @@
                     <option id="zdr5" value="moste1"> dr. med. Jernej Novak </option>
                     <option id="zdr6" value="moste2"> dr. med. Ana Horvat </option>
                   </select>
-              </div>  
+                  </div>
             </div>
+        </div>
+          
+        <div class="container">
             
-          <div class="panel-group" id="narocilnice">
-              
-          </div>
-          
-    	  <div id='calendar' class="koledar-zdravnik"></div>
-          
+          <div class="panel-group" id="narocilnice"></div>
+        
         </div>
         
+        <div class="container">
+        
+          <div id="wrap" class="panel odmik">
+            
+            <div id='calendar' class="koledar-zdravnik"></div>
+            
+            <div style='clear:both'></div>
+            
+          </div>
+          
+       
+        
+        <div class="panel panel-default" id="noga">
+            <div class="panel-heading">
+                <div class="row">
+                  <div class="col-md-10">
+                      <h5>&copy AppMojstri</h5>
+                  </div>
+                </div>  
+            </div>
+        </div>
+        
+         </div>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
