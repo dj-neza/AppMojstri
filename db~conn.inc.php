2e0e13daccfaed6a47069cefcf164665d557e6d8
Gregor Leskovšek <legrega@gmail.com>
2016-07-15 02:21:20 +0200

    Baza sedaj dostopna tudi od zunaj

--- db~conn.inc.php
+++ db~conn.inc.php
@@ -4,7 +4,7 @@ if(basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
 		header("location: index.php");
 }
 
-$conn = new mysqli("localhost", "appmojster", "mArAnd%2016", "appmojstri");
+$conn = new mysqli("93.103.24.231", "appmojster", "mArAnd%2016", "appmojstri");
 if ($conn->connect_errno) {
 		echo "Failed to connect to MySql: " . $conn->connect_error;
 }
